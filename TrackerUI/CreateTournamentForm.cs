13849a73b3d8bd5d5f203b61a50ea56d61b8751f
dammalk <dammalk@gmail.com>
2021-06-30 16:03:58 +0900

    Emailing Users

--- TrackerUI/CreateTournamentForm.cs
+++ TrackerUI/CreateTournamentForm.cs
@@ -136,6 +136,8 @@ namespace TrackerUI
             // Create all of the teams entries
             GlobalConfig.Connection.CreateTournament(tm);
 
+            tm.AlertUsersToNewRound();
+
             TournamentViewerForm frm = new TournamentViewerForm(tm);
             frm.Show();
             this.Close();
